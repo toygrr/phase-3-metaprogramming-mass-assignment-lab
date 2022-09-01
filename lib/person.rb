c121ed91cc10404e22a8700dfce9d7118a1dc80b
Tyler <toygrrr@gmail.com>
2022-09-01 09:20:50 -0700

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    # iterating over hash to create a person class for each
+    attributes.each do |key, value|
+      # for each hash, creating a class method (attr_accessor) and applying it to that person class
+      self.class.attr_accessor(key)
+      # sending the data to fill in key/value pairs in our new person class
+      self.send("#{key}=", value)
+    end
+  end
 end
